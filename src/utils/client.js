2825af9e34acab9949946a2f550a7f3518ec974f
Rupert Caingal <johnrupertcaingal@gmail.com>
2024-03-11 17:41:20 +0800

    Updated directory

--- src/utils/client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prismaClienSingleton = () => {
-    return new client_1.PrismaClient();
-};
-const globalForPrisma = globalThis;
-const prisma = globalForPrisma !== null && globalForPrisma !== void 0 ? globalForPrisma : prismaClienSingleton();
-exports.default = prisma;
